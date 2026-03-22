bd8aad8b8a9e4dbd3ac0808a8406a5af2a1a6cd9
Farmerfuzzys <jaemy.jcb.b.c.j@gmail.com>
2026-03-22 18:18:13 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDiYOJWP4XMQRJ6b-RfJp6Aqxis7E-uA30",
+  authDomain: "farmerfuzzys-c32eb.firebaseapp.com",
+  projectId: "farmerfuzzys-c32eb",
+  storageBucket: "farmerfuzzys-c32eb.firebasestorage.app",
+  messagingSenderId: "842395157432",
+  appId: "1:842395157432:web:fd031d7cc0c2a6e69ec54d"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
